1d8491fbdfcb9f78ff7e926d715183bfe7155c35
weru <weru.marsha@gmail.com>
2023-11-22 04:26:14 -0800

    Position the nav bar

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,9 +3,12 @@ import { logo } from '../assets';
 const hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-        <nav className="flex justify-between items-center flex-col">
+        <nav className="flex justify-between items-center w-full mb-10 pt-3">
             <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
-            <button></button>
+
+            <button type="button" onClick={() => window.open('https://github.com/marshaweru')} className="black_btn">
+                Github
+            </button>
         </nav>
     </header>
   )
