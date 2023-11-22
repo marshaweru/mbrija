68643d3e23a1a03321d8e6248d4c4fe3978ffe69
weru <weru.marsha@gmail.com>
2023-11-22 04:10:25 -0800

    Add button to point to github repo

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,7 +3,10 @@ import { logo } from '../assets';
 const hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-
+        <nav className="flex justify-between items-center flex-col">
+            <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+            <button></button>
+        </nav>
     </header>
   )
 }
