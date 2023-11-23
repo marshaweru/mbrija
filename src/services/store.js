bf266c9df14675550ef3e80b9843b00411bcb79a
weru <weru.marsha@gmail.com>
2023-11-23 04:37:11 -0800

    Add file:one specific part of a state of a global store

--- src/services/store.js
+++ src/services/store.js
@@ -2,4 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
     reducer: {},
-})
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat()
+});
