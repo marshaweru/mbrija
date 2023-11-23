61a6ecacd154fe0e1211901d67b93232efdb2d83
weru <weru.marsha@gmail.com>
2023-11-23 03:13:59 -0800

    Input where all logic will be connected

--- src/components/demo.jsx
+++ src/components/demo.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+
+import { copy, linkIcon, loader, tick } from '../assets';
 
 const demo = () => {
   return (
-    <div>demo</div>
+    <section className="mt-16 w-full max-w-xl">
+      {/*search*/}
+      <div className="flex flex-col w-full gap-2">
+        <form className="relative flex justify-center items-center" onSubmit={() =>{}}>
+          <img src="linkIcon" alt="link_icon" className="absolute left-0 my-2 ml-3 w-5"/>
+          <input type="text" />
+        </form>
+      </div>
+
+    </section>
   )
 }
 
