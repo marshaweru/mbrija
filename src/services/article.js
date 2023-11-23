4b4ebd4ad9ece67c84e4243be0ff750bc55c3bf9
weru <weru.marsha@gmail.com>
2023-11-23 05:33:56 -0800

    Paste the API

--- src/services/article.js
+++ src/services/article.js
@@ -1,5 +1,28 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+  params: {
+    url: 'https://time.com/6266679/musk-ai-open-letter/',
+    length: '3'
+  },
+  headers: {
+    'X-RapidAPI-Key': '6afc9cf076msh82df439f7636919p151941jsn7352194bdc44',
+    'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
+
+
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
