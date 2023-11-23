7f40482bbdff23dba8d5fba4d65b8905e88a7775
weru <weru.marsha@gmail.com>
2023-11-23 04:49:13 -0800

    Import store

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
 import App from './App'
-
+import { store }
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider>
