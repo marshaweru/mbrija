27ad8da2c5f4fa6416a0a7d7fe67c099945e565d
weru <weru.marsha@gmail.com>
2023-11-23 05:18:10 -0800

    Add forward slash
    undefined

--- src/services/article.js
+++ src/services/article.js
@@ -2,6 +2,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const articleApi = createApi({
     reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: ''
+    })
     endpoints: (builder) => ({
         getSummary: builder.query({
             query: (params) => 'test'
