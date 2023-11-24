fb2ebb6da6e50615405c3c3191bb938694653df6
weru <weru.marsha@gmail.com>
2023-11-24 07:46:34 -0800

    Replace API with new const

--- src/services/article.js
+++ src/services/article.js
@@ -2,18 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const axios = require('axios');
 
-const options = {
-  method: 'GET',
-  url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
-  params: {
-    url: 'https://time.com/6266679/musk-ai-open-letter/',
-    length: '3'
-  },
-  headers: {
-    'X-RapidAPI-Key': '6afc9cf076msh82df439f7636919p151941jsn7352194bdc44',
-    'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
-  }
-};
+const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY
 
 try {
 	const response = await axios.request(options);
