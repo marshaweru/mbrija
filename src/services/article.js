8b41350f22b56565078322fadd4142b05a13bf5f
weru <weru.marsha@gmail.com>
2023-11-24 07:55:58 -0800

    Create the end point

--- src/services/article.js
+++ src/services/article.js
@@ -2,28 +2,22 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const axios = require('axios');
 
-const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
-
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY
 
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: ( headers ) => {
-          headers.set('X-RapidAPI-Key', 'KEY');
+          headers.set('X-RapidAPI-Key', rapidApiKey);
           headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
+          return headers;
         }
     }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-            query: (params) => 'test'
+            query: (params) => /summarize
         })
     })
 });
