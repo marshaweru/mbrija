a9e9d8e4bafd122e91594dd549fe31e414ae483a
weru <weru.marsha@gmail.com>
2023-11-28 08:33:59 -0800

    Delete misplaced code line

--- src/services/article.js
+++ src/services/article.js
@@ -1,7 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const axios = require('axios');
-
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY
 
 export const articleApi = createApi({
