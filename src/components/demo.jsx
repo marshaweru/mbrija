9b8fa4133090889ea48141427004b7d1058b49fe
weru <weru.marsha@gmail.com>
2023-11-28 07:58:08 -0800

    Correct spelling error

--- src/components/demo.jsx
+++ src/components/demo.jsx
@@ -40,7 +40,7 @@ const handleSubmit = async (e) => { e.preventDefault();
 const handleCopy = (copyUrl) => {
   setCopied(copyUrl);
   navigator.clipboard.writeText(copyUrl);
-  
+  setTimeout{() => setCopied(false), 5000);
 }
 
   return (
@@ -58,8 +58,9 @@ const handleCopy = (copyUrl) => {
         </form>
         <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">{allArticles.map((item, index) => {
           <div key={`link-${index}`}onClick={() => setArticle(item)} className="link_card">
-            <div className="copy_btn">
-              <img src={copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain"/>
+            <div className="copy_btn" onClick={() =>
+            handleCopy(item.url)}>
+              <img src={copied === item.url ? tick: copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain"/>
             </div>
             <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
           </div>
