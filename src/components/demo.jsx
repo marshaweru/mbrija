0b3e870427b4f56b942df50fe92e09cbfac4fe7e
weru <weru.marsha@gmail.com>
2023-11-29 04:49:32 -0800

    Proper indentation

--- src/components/demo.jsx
+++ src/components/demo.jsx
@@ -100,7 +100,9 @@ const handleKeyDown = (e) => {
                 className='w-[40%] h-[40%] object-contain'
               />
             </div>
-            <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
+            <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+              {item.url}
+            </p>
           </div>
         ))}
         </div>
