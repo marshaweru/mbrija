edc7ff23899842992a9603b8a1ee78224882e1de
weru <weru.marsha@gmail.com>
2023-11-29 04:46:05 -0800

    Indent properly

--- src/components/demo.jsx
+++ src/components/demo.jsx
@@ -93,9 +93,12 @@ const handleKeyDown = (e) => {
             onClick={() => setArticle(item)} 
             className='link_card'
           >
-            <div className='copy_btn' onClick={() =>
-            handleCopy(item.url)}>
-              <img src={copied === item.url ? tick : copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain"/>
+            <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+              <img 
+              src={copied === item.url ? tick : copy} 
+              alt="copy_icon" 
+              className="w-[40%] h-[40%] object-contain"
+              />
             </div>
             <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
           </div>
