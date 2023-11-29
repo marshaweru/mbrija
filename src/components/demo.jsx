7497eb0b3d364bb4a3b369b2ce7a8083d7374676
weru <weru.marsha@gmail.com>
2023-11-29 04:25:02 -0800

    Indent properly

--- src/components/demo.jsx
+++ src/components/demo.jsx
@@ -67,7 +67,8 @@ const handleKeyDown = (e) => {
             className='absolute left-0 my-2 ml-3 w-5'
           />
 
-          <input type="url" placeholder="Enter a URL" value={article.url} onChange={(e) => setArticle({ ...article, url: e.target.value })} required className="url_input peer"/>
+          <input 
+            type="url" placeholder="Enter a URL" value={article.url} onChange={(e) => setArticle({ ...article, url: e.target.value })} required className="url_input peer"/>
 
           <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">
 
