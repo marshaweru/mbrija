bc3bb0c5573f0bf72b5fe679e0b2ae123dee3bfe
weru <weru.marsha@gmail.com>
2023-11-29 04:25:47 -0800

    Put single quotes

--- src/components/demo.jsx
+++ src/components/demo.jsx
@@ -68,7 +68,7 @@ const handleKeyDown = (e) => {
           />
 
           <input 
-            type="url" placeholder="Enter a URL" value={article.url} onChange={(e) => setArticle({ ...article, url: e.target.value })} required className="url_input peer"/>
+            type='url' placeholder="Enter a URL" value={article.url} onChange={(e) => setArticle({ ...article, url: e.target.value })} required className="url_input peer"/>
 
           <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">
 
